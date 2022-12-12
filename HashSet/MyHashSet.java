3cef7621dc11f98a02d7116f32c5579e8862f032
UA-Igor <113474981+UA-Igor@users.noreply.github.com>
2022-12-13 00:57:16 +0200

    Update MyHashSet.java

--- HashSet/MyHashSet.java
+++ HashSet/MyHashSet.java
@@ -4,9 +4,10 @@ public class MyHashSet<T>{
 
     private int cacheLength;
     private Object[] arr;
+    private int[] elementsLength;
     private static final Object dummy = new Object();
-    private final int TREEFY_THRESHOLD = 8;
-    private final int TREEFY_COLLAPSE = 4;
+    private final int TREEIFY_THRESHOLD = 8;
+    private final int TREEIFY_COLLAPSE = 4;
 
     MyHashSet(){
         this.cacheLength = 20;
@@ -33,7 +34,7 @@ public class MyHashSet<T>{
         }
         else if(arr[collapsedHash].getClass().getName().equals("java.util.LinkedList")){
             List<T> list = (LinkedList<T>)arr[collapsedHash];
-            if(list.size() < TREEFY_THRESHOLD){
+            if(list.size() < TREEIFY_THRESHOLD){
                 return list.add(data);
             }
             else{
@@ -84,7 +85,7 @@ public class MyHashSet<T>{
             }
             else if(arr[collapsedHash].getClass().getName().equals("java.util.TreeMap")){
                 TreeMap<T, Object> map = (TreeMap)arr[collapsedHash];
-                if(map.size() > TREEFY_COLLAPSE){
+                if(map.size() > TREEIFY_COLLAPSE){
                     map.remove(data);
                     return true;
                 }
@@ -109,42 +110,67 @@ public class MyHashSet<T>{
     public void print(){
         int i = 0;
         List<T> list;
+        TreeMap<T, Object> map;
         for(Object x: arr){
             if(x != null) {
                 if (x.getClass().getName().equals("java.util.LinkedList")) {
                     list = (LinkedList)x;
+                    for(T t: list){
+                        System.out.print("[" + t.toString() + "]");
+                        if(i % 20 == 0){
+                            System.out.print('\n');
+                        }
+                        ++i;
+                    }
                 }
                 else{
-                    list = listing((TreeMap<T, Object>)x);
-                }
-                for(T t: list){
-                    System.out.print("[" + t.toString() + "]");
-                    if(i % 20 == 0){
-                        System.out.print('\n');
+                    map = (TreeMap<T, Object>)x;
+                    for(T t: map.keySet()){
+                        System.out.print("[" + t.toString() + "]");
+                        if(i % 20 == 0){
+                            System.out.print('\n');
+                        }
+                        ++i;
                     }
-                    ++i;
                 }
             }
         }
     }
 
     public T get(int index){
-        int i = 0;
+        int pointer = 0;
         List<T> list;
+        TreeMap<T, Object> map;
         for(Object x: arr){
             if(x != null) {
+                //if index greater than list or map size in current array cell, skip it
                 if (x.getClass().getName().equals("java.util.LinkedList")) {
                     list = (LinkedList)x;
+                    if(index > list.size() - 1){
+                        pointer = list.size() - 1;
+                        continue;
+                    }
+                    for(T t: list){
+                        if(pointer == index){
+                            return t;
+                        }
+                        ++pointer;
+                    }
                 }
                 else{
-                    list = listing((TreeMap<T, Object>)x);
-                }
-                for(T t: list){
-                    if(i == index){
-                        return t;
+                    map = (TreeMap<T, Object>)x;
+                    if(index > map.size() - 1){
+                        pointer = map.size() - 1;
+                        continue;
                     }
-                    ++i;
+                    for(T t: map.keySet()){
+                        if(pointer == index){
+                            return t;
+                        }
+                        ++pointer;
+                    }
                 }
+
             }
         }
         return null;
